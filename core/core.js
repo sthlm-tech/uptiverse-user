37508ae8e77cae0e73c2b1f0539151ce6d83dc94
ulfdahlstrand <ulf.d.dahlstrand@gmail.com>
2016-10-23 18:54:37 +0200

    added eureka

--- core/core.js
+++ core/core.js
@@ -3,6 +3,7 @@ var bodyParser = require("body-parser");
 var config = require("./../config");
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var Eureka = require('eureka-js-client').Eureka;
 /*
 passport.use(new GoogleStrategy({
     clientID: "524442664999-jom04q4ea3erjt4fr84m3cf1sbojalhd.apps.googleusercontent.com",
@@ -28,6 +29,27 @@ var App = {
 		App.Server = App.Express.listen(process.env.PORT || config.port, function() {
 		    console.log("Listening on port %d", App.Server.address().port);
 		});
+
+		App.Eureka = new Eureka({
+      // application instance information
+      instance: {
+        app: 'uptiverse-user',
+        hostName: 'localhost',
+        ipAddr: '127.0.0.1',
+        port: 8080,
+        vipAddress: 'jq.test.something.com',
+        dataCenterInfo: {
+          name: 'MyOwn',
+        },
+      },
+      eureka: {
+        // eureka server host / port
+        host: '172.17.25.70',
+        port: 32768,
+      },
+    });
+
+      App.Eureka.start();
 	}
 };
 
